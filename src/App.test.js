c1df27cdc60a06eaae1ff24e5486e001536fd6b5
Bleriot Noguia <nstevebleriot@yahoo.fr>
2022-12-23 19:59:34 +0100

    Test and code function to transform camel case colors to speced colors

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import { replaceCamelWithSpaces } from './App'
 
 test("button has correct initial color", () => {
   render(<App />);
@@ -83,4 +84,16 @@ test('Clicked disabled button has gray background and reverts to blue', () => {
   fireEvent.click(checkbox)
   expect(button).toHaveStyle({backgroundColor: 'blue'})
 
+})
+
+describe('spaces before camel-case capital letters', () => {
+  test('Works for no inner capital letters', () => {
+    expect(replaceCamelWithSpaces('Red')).toBe('Red')
+  });
+  test('Works for one inner capital letter', () => {
+    expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue')
+  });
+  test('Works for multiple inner capital letters', () => {
+    expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red')
+  })
 })
